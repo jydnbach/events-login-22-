02bf8d2565580ca64d8864d4052bfa3e8844fde7
jydnbach <jaydenkim95@gmail.com>
2024-06-23 20:09:46 +0900

    show/unshow menus with token logic

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,9 +1,10 @@
-import { NavLink } from 'react-router-dom';
-import { Form } from 'react-router-dom';
+import { Form, NavLink, useRouteLoaderData } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 import NewsletterSignup from './NewsletterSignup';
 
 function MainNavigation() {
+  const token = useRouteLoaderData('root');
+
   return (
     <header className={classes.header}>
       <nav>
@@ -39,19 +40,23 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              to="/auth?mode=login"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Authentication
-            </NavLink>
-          </li>
-          <Form action="/action" method="post">
-            <button>Logout</button>
-          </Form>
+          {!token && (
+            <li>
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Authentication
+              </NavLink>
+            </li>
+          )}
+          {token && (
+            <Form action="/action" method="post">
+              <button>Logout</button>
+            </Form>
+          )}
         </ul>
       </nav>
       <NewsletterSignup />
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -2,3 +2,9 @@ export function getAuthToken() {
   const token = localStorage.getItem('token');
   return token;
 }
+
+export function tokenLoader() {
+  return getAuthToken();
+}
+
+export function checkAuthLoader() {}
